f58e068fdf7b5341c502eb57c48286dd23aa978b
Rogerio Gelonezi <rogeriogelonezi@gmail.com>
2024-04-28 17:03:08 -0300

    Setup Inicial do Projeto

--- src/apps/imoveis/views.py
+++ src/apps/imoveis/views.py
@@ -4,4 +4,4 @@ from apps.imoveis.models import Imovel
 
 def index(request):
     imoveis = Imovel.objects.order_by("titulo")
-    return render(request, "imoveis/index.html", {"cards": imoveis})
+    return render(request, "shared/bem-vindo.html", {"cards": imoveis})
