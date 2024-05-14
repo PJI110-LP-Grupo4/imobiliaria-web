9e1274dd28942dd58f347d9b8ee049823c049b77
mauro <mauro_leao@hotmail.com>
2024-05-14 12:44:26 -0300

    Usuarios-teste teste

--- src/apps/usuarios/models.py
+++ src/apps/usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    STATUS_CHOICES = [
+        (1,'Feminino'),
+        (2,'Masculino'),
+        (3,'Não Informar')
+        
+    ]
+    nome = models.CharField(max_length=100, null=False)
+    sobrenome = models.CharField(max_length=100, null=False)
+    data = models.DateField(null=False, blank=False)
+    status = models.IntegerField(choices=STATUS_CHOICES, null=False, blank=False)
--- src/apps/usuarios/views.py
+++ src/apps/usuarios/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import UsuarioForm
+from .models import Usuario
 
 # Create your views here.
+def cadastrar_usuario(request):
+    if request.method == "POST":
+        form_usuario = UsuarioForm(request.POST)
+        if form_usuario.is_valid():
+            form_usuario.save()
+            return redirect('index')
+            
+    else:
+
+        form_usuario = UsuarioForm()
+    return render(request, 'form_cadastro.html', {'form_usuario': form_usuario} )
+
+
+
+def listar_usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'listar_usuarios.html', {'usuarios': usuarios} )
