faa04f6b51d80741348c7368fc01834daacd2f4c
Rogerio Gelonezi <rogeriogelonezi@gmail.com>
2024-05-18 11:25:14 -0300

    Versão Inicial do Enriquecimento da Modelagem de Classes

--- src/apps/imoveis/models.py
+++ src/apps/imoveis/models.py
@@ -2,6 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=25, null=False, blank=False, unique=True)
+
+
+ESTADOS = [
+    ("SP", "São Paulo"),
+    ("RJ", "Rio de Janeiro"),
+    ("DF", "Distrito Federal"),
+    ("MG", "Minas Gerais"),
+]
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    estado = models.CharField(max_length=2, null=False, choices=ESTADOS, default="")
+
+
 class Imovel(models.Model):
     titulo = models.CharField(max_length=25, null=False, blank=False)
     resumo = models.CharField(max_length=50, null=False, blank=False)
@@ -15,4 +32,11 @@ class Imovel(models.Model):
         null=True,
         blank=False,
         related_name="user",
+    )
+    categoria = models.ForeignKey(
+        to=Categoria,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="categoria",
     )
