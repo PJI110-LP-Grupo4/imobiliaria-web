c44e38823c2c8e21b8a9f5ad858e006f8854523f
mauro <mauro_leao@hotmail.com>
2024-05-18 14:02:29 -0300

    Elaboração do formulario de login

--- src/apps/usuarios/models.py
+++ src/apps/usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    STATUS_CHOICES = [
+        (1, 'Comprador'),
+        (2, 'Vendedor')
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.CharField(max_length=100, null=False, blank=False)
+    senha = models.CharField(max_length=10, null=False, blank=False)
+    confirme_senha = models.CharField(max_length=10, null=False, blank=False)
+    status = models.IntegerField(choices=STATUS_CHOICES, null=False, blank=False)
