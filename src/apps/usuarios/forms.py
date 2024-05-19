4d7a5a46e280c7a25ae2a16a0b916db892ebb9ac
Rogerio Gelonezi <rogeriogelonezi@gmail.com>
2024-05-19 18:46:11 -0300

    Implementação da Camada de Usuarios

--- src/apps/usuarios/forms.py
+++ src/apps/usuarios/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class EntrarForms(forms.Form):
@@ -24,3 +25,59 @@ class EntrarForms(forms.Form):
             }
         ),
     )
+
+
+class NovoUsuarioForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Cadastro e Exibição",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: Imobiliária Web",
+            }
+        ),
+    )
+    email = forms.EmailField(
+        label="E-mail",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: contato@imobiliariaweb.com.br",
+            }
+        ),
+    )
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite a sua senha",
+            }
+        ),
+    )
+    senha_2 = forms.CharField(
+        label="Confirme a sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Confirme a sua senha",
+            }
+        ),
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        senha_1 = cleaned_data.get("senha_1")
+        senha_2 = cleaned_data.get("senha_2")
+        if senha_1 and senha_2 and senha_1 != senha_2:
+            self.add_error(
+                "senha_2", ValidationError("As senhas digitadas precisam ser iguais")
+            )
---
+++ src/helpers/error_helper.py
@@ -0,0 +1,6 @@
+from django.contrib import messages
+
+def tratar_errors(form, request):
+    for field in form:
+        for error in field.errors:
+            messages.warning(request, error)
